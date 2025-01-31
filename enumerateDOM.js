ab18e68cfc8ef0e11ac9fd87cb92f4afc8b0f8d2
Hackertips.Today <mgreen12206@gmail.com>
2025-01-30 23:30:58 -0500

    Create enumerateDOM.js

---
+++ enumerateDOM.js
@@ -0,0 +1,33 @@
+
+  _______                                       __             ______                  
+ |   _   .-----.--.--.--------.-----.----.---.-|  |_.-----.   |   _  \ .-----.--------.
+ |.  1___|     |  |  |        |  -__|   _|  _  |   _|  -__|   |.  |   \|  _  |        |
+ |.  __)_|__|__|_____|__|__|__|_____|__| |___._|____|_____|   |.  |    |_____|__|__|__|
+ |:  1   |                                                    |:  1    /               
+ |::.. . |          [ Enumerate DOM ]                         |::.. . /                
+ `-------'                                                    `------'                 
+                                                                                       
+
+// Paste into console
+window._details = [
+    'iframe', 'frame', 'object', 'embed', 'applet', 'form', 'meta', 'script',
+    'link', 'style', 'video', 'audio', 'canvas', 'svg', 'source', 'picture',
+    'noscript', 'base', 'img', 'track', 'param', 'map', 'area', 'marquee',
+    'body', 'head', 'div', 'span', 'section', 'article', 'aside', 'header',
+    'footer', 'nav', 'main', 'details', 'summary', 'template', 'shadow',
+    'slot', 'data', 'time', 'meter', 'progress', 'output', 'wbr', 'dialog',
+    'button', 'input', 'textarea', 'select', 'option', 'optgroup', 'datalist'
+].map(type => 
+    Array.from(document.querySelectorAll(type)).map((el, index) => 
+        `Type: ${type}, Src: ${el.src || 'No src'}, ID: ${el.id || 'No ID, index ' + index}, 
+        Class: ${el.className || 'No class'}, HTML: ${el.outerHTML.slice(0, 200)}`
+    )
+).flat();
+
+if (window._details.length) {
+    console.log("Detected framing elements and potential security risks:");
+    window._details.forEach(detail => console.log(detail));
+} else {
+    console.log("No common framing or embedded elements detected.");
+}
+
