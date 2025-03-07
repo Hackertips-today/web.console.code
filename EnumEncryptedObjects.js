36d14f463fde6b89d0486479669451d4df694668
Hackertips.Today <mgreen12206@gmail.com>
2025-03-07 13:01:37 -0500

    Update EnumEncryptedObjects.js
    Updated: 03-07-25

--- EnumEncryptedObjects.js
+++ EnumEncryptedObjects.js
@@ -1,7 +1,20 @@
-// This script will attempt to enumerate encrypted objects within the DOM
+/*
+Filename: AllUA.js
+Purpose: Define an array of user agents which can be used for many things
 
+Note: This Javascript snippet is meant for Developer Tools (F12) 
+ / Inspect Element / Console tab
+ Once in the Console Tab you will see messages, look at the bottom for >
+To verify you are in the correct spot, you can list all the objects:
+type in at the > prompt: 
+ dir(window);
+If you cannot type, you may have to type: 
+> allow pasting 
+(to be able to type at the console) - its not always mandatory.
 
-
+Paste the script in the console at the > prompt
+*/
+    
 (function findEncryptedGlobals() {
     const globalObjects = Object.keys(window); // Get all global properties
 
@@ -52,3 +65,11 @@
     console.log("Potentially Encrypted Objects:", encryptedObjects);
 })();
 
+
+/*
+Example output:
+VM261:48 Potentially Encrypted Objects: (33) ['sca', 'kta', 'lta', 'mta', 'nta', 'ota', 'pta', 'qta', 'rta', 'sta', 'tta', 'uta', 'vta', 'wta', 'xta', 'yta', 'zta', 'Ata', 'Bta', 'Cta', 'Dta', 'Eta', 'Fta', 'Gta', 'Hta', 'Ita', 'Jta', 'Kta', 'vIa', 'E9a', 'Cnb', 'Ewb', 'Fwb']0: "sca"1: "kta"2: "lta"3: "mta"4: "nta"5: "ota"6: "pta"7: "qta"8: "rta"9: "sta"10: "tta"11: "uta"12: "vta"13: "wta"14: "xta"15: "yta"16: "zta"17: "Ata"18: "Bta"19: "Cta"20: "Dta"21: "Eta"22: "Fta"23: "Gta"24: "Hta"25: "Ita"26: "Jta"27: "Kta"28: "vIa"29: "E9a"30: "Cnb"31: "Ewb"32: "Fwb"length: 33[[Prototype]]: Array(0)
+12:58:09.688 undefined
+
+Remember: undefined means a function (one of them) didnt return anything
+*/
